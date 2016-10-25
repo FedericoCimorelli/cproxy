8bdd9b6dd34c6fe452631a1c35debcf842ebed6c
Federico Cimorelli <cimorellifederico@gmail.com>
2016-10-25 11:28:50 +0200

    -

--- getstats.sh
+++ getstats.sh
@@ -1,5 +1,9 @@
  #!/bin/bash
-sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/flowmod_latency_C1.csv ./flowmod_latency_C1.csv
-sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/flowmod_latency_C2.csv ./flowmod_latency_C2.csv
-sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/flowmod_latency_C3.csv ./flowmod_latency_C3.csv
-sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/wardrop.csv ./wardrop.csv
+sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/lb1_flowmod_latency_C1.csv ./lb1_flowmod_latency_C1.csv
+sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/lb1_flowmod_latency_C2.csv ./lb1_flowmod_latency_C2.csv
+sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/lb1_flowmod_latency_C3.csv ./lb1_flowmod_latency_C3.csv
+sshpass  -p "odl" scp -P 2004 odl@172.27.1.4:/home/odl/cproxy/lb1_wardrop.csv ./lb1_wardrop.csv
+sshpass  -p "odl" scp -P 2005 odl@172.27.1.4:/home/odl/cproxy/lb2_flowmod_latency_C1.csv ./lb2_flowmod_latency_C1.csv
+sshpass  -p "odl" scp -P 2005 odl@172.27.1.4:/home/odl/cproxy/lb2_flowmod_latency_C2.csv ./lb2_flowmod_latency_C2.csv
+sshpass  -p "odl" scp -P 2005 odl@172.27.1.4:/home/odl/cproxy/lb2_flowmod_latency_C3.csv ./lb2_flowmod_latency_C3.csv
+sshpass  -p "odl" scp -P 2005 odl@172.27.1.4:/home/odl/cproxy/lb2_wardrop.csv ./lb2_wardrop.csv
